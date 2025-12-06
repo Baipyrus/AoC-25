2c1f31abe33c02999a6ab6aeb30d80b5073de953
Baipyrus <simon.waltemathe@gmail.com>
2025-12-07 00:59:38 +0100

    chore: update challenge day01 part2 with solution of part1

--- internal/day01/part2/main.go
+++ internal/day01/part2/main.go
@@ -3,6 +3,7 @@ package day01_part2
 import (
 	"fmt"
 
+	"github.com/Baipyrus/AoC-25/internal/day01"
 	"github.com/Baipyrus/AoC-25/internal/registry"
 )
 
@@ -15,8 +16,21 @@ func init() {
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
 
-	var (
-		dial   uint
-		zeroes uint
-	)
+	var zeroes uint
+	instructions, dial := day01.ParseInput(input)
+
+	for _, inst := range instructions {
+		// Reverse steps if DIRECTION == LEFT
+		steps := int64(inst.Steps) * int64(inst.Dir)
+
+		// Dial rotation wrap-around
+		dial = uint((int64(dial) + steps + 100) % 100)
+
+		// Detect zero positions for use in password
+		if dial == 0 {
+			zeroes++
+		}
+	}
+
+	fmt.Printf("The password to open the door is: '%d'", zeroes)
 }
