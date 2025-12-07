8de65eba7b2f1267455e8f9f67229e1fed705fd8
Baipyrus <simon.waltemathe@gmail.com>
2025-12-07 18:21:48 +0100

    chore: better conversion of measured durations as human readable

--- main.go
+++ main.go
@@ -25,10 +25,33 @@ func main() {
 	start := time.Now()
 	exec(input)
 
-	stop := time.Since(start).Microseconds()
-	total := time.Since(entry).Milliseconds()
+	stop := time.Since(start)
+	total := time.Since(entry)
 
 	fmt.Println("\nExecution time recorded at:")
-	fmt.Printf("Main: %d ms\n", total)
-	fmt.Printf("Challenge: %d μs\n", stop)
+	fmt.Printf("Main: %s\n", formatDuration(total))
+	fmt.Printf("Challenge: %s\n", formatDuration(stop))
+}
+
+// Converts into the most appropriate unit string for human readability.
+func formatDuration(d time.Duration) string {
+	if d >= time.Second {
+		// Use d.Seconds() which returns float64
+		return fmt.Sprintf("%.4fs", d.Seconds())
+	}
+
+	if d >= time.Millisecond {
+		// Divide by the Millisecond constant to get a precise float value
+		ms := float64(d) / float64(time.Millisecond)
+		return fmt.Sprintf("%.4fms", ms)
+	}
+
+	if d >= time.Microsecond {
+		// Divide by the Microsecond constant
+		us := float64(d) / float64(time.Microsecond)
+		return fmt.Sprintf("%.4fµs", us)
+	}
+
+	// Default to Nanoseconds
+	return fmt.Sprintf("%dns", d.Nanoseconds())
 }
