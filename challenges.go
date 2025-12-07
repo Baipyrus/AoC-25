f476356b00a3afc01fcdea65a29b711eace87896
Baipyrus <simon.waltemathe@gmail.com>
2025-12-07 17:00:36 +0100

    feat: initialize challenge day02 part1

--- challenges.go
+++ challenges.go
@@ -3,4 +3,5 @@ package main
 import (
 	_ "github.com/Baipyrus/AoC-25/internal/day01/part1"
 	_ "github.com/Baipyrus/AoC-25/internal/day01/part2"
+	_ "github.com/Baipyrus/AoC-25/internal/day02/part1"
 )
---
+++ internal/day02/part1/main.go
@@ -0,0 +1,17 @@
+package day02_part1
+
+import (
+	"fmt"
+
+	"github.com/Baipyrus/AoC-25/internal/registry"
+)
+
+var name = "Day 02 - Part 1"
+
+func init() {
+	registry.Register(name, Main)
+}
+
+func Main(input string) {
+	fmt.Printf("Executing: %s\n", name)
+}
