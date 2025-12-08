5799e076508bb25df367e67b93876b1a1c642c7c
Baipyrus <simon.waltemathe@gmail.com>
2025-12-08 20:04:10 +0100

    feat: implement solution for challenge day03 part2

--- internal/day03/part2/main.go
+++ internal/day03/part2/main.go
@@ -18,25 +18,51 @@ func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
 
 	var joltageSum uint64
+
+	// WARNING: Logically speaking, this value should only ever
+	//          go as low as the length of 2.
+	// NOTE: If you *do* set the valaue to 2, you should expect
+	//       the same result as from day03_part1!
+	combinationSize := 12
+
 	batteryBanks := day03.ParseInput(input)
 
 	for _, batteryBank := range batteryBanks {
 		batteries := batteryBank.Batteries
 
 		var maxJoltage uint64
-		for i := 0; i < len(batteries)-1; i++ {
-			leftBattery := fmt.Sprint(batteries[i].Joltage)
+		// We want a combination of length 12. Meaning the last
+		// possible position for the first number is index at
+		// length len - (12 - 1) because we want 11 more numbers.
+		for i := 0; i < len(batteries)-(combinationSize-1); i++ {
+			combinedBatteries := fmt.Sprint(batteries[i].Joltage)
+			lastIndex := i
 
-			for j := i + 1; j < len(batteries); j++ {
-				rightBattery := fmt.Sprint(batteries[j].Joltage)
+			// We want to choose 11 more digits. We need to subtract
+			// 1 because it's an index, not a length, and another 1
+			// because we already have one digit ready.
+			for j := combinationSize - 2; j >= 0; j-- {
+				var largestBattery uint64
 
-				// Combine the left and right batteries to get joltage
-				joltage, _ := strconv.ParseUint(leftBattery+rightBattery, 10, 64)
+				// The last possible digit would be closest to
+				// the end but offset by j positions to leave
+				// enough room for the other digits
+				for k := lastIndex + 1; k < len(batteries)-j; k++ {
+					currentBattery := batteries[k].Joltage
 
-				// Compare with last maximum joltage
-				if joltage > maxJoltage {
-					maxJoltage = joltage
+					if currentBattery > largestBattery {
+						largestBattery = currentBattery
+						lastIndex = k
+					}
 				}
+
+				combinedBatteries += fmt.Sprint(largestBattery)
+			}
+
+			// Lastly, convert the newly found combined jotage and compare it
+			currentJoltage, _ := strconv.ParseUint(combinedBatteries, 10, 64)
+			if currentJoltage > maxJoltage {
+				maxJoltage = currentJoltage
 			}
 		}
 
