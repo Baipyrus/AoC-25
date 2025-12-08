ad6b9dba74dc07e609265634b4706c65b75274fc
Baipyrus <simon.waltemathe@gmail.com>
2025-12-08 18:34:45 +0100

    chore: update challenge day03 part2 with solution of part1

--- internal/day03/part2/main.go
+++ internal/day03/part2/main.go
@@ -2,7 +2,9 @@ package day03_part2
 
 import (
 	"fmt"
+	"strconv"
 
+	"github.com/Baipyrus/AoC-25/internal/day03"
 	"github.com/Baipyrus/AoC-25/internal/registry"
 )
 
@@ -14,4 +16,32 @@ func init() {
 
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
+
+	var joltageSum uint64
+	batteryBanks := day03.ParseInput(input)
+
+	for _, batteryBank := range batteryBanks {
+		batteries := batteryBank.Batteries
+
+		var maxJoltage uint64
+		for i := 0; i < len(batteries)-1; i++ {
+			leftBattery := fmt.Sprint(batteries[i].Joltage)
+
+			for j := i + 1; j < len(batteries); j++ {
+				rightBattery := fmt.Sprint(batteries[j].Joltage)
+
+				// Combine the left and right batteries to get joltage
+				joltage, _ := strconv.ParseUint(leftBattery+rightBattery, 10, 64)
+
+				// Compare with last maximum joltage
+				if joltage > maxJoltage {
+					maxJoltage = joltage
+				}
+			}
+		}
+
+		joltageSum += maxJoltage
+	}
+
+	fmt.Printf("The sum total of all battery's max joltages is: %d\n", joltageSum)
 }
