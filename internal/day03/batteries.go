b209c3a223e7dad79415f8d5759f3cc80cac7010
Baipyrus <simon.waltemathe@gmail.com>
2025-12-08 19:20:40 +0100

    chore: debug string formatter for challenge day03 part2

--- internal/day03/batteries.go
+++ internal/day03/batteries.go
@@ -1,5 +1,7 @@
 package day03
 
+import "fmt"
+
 type Battery struct {
 	Joltage uint64
 	Index   int
@@ -8,4 +10,12 @@ type Battery struct {
 type BatteryBank struct {
 	Batteries  []Battery
 	MaxJoltage uint64
+}
+
+func (bank BatteryBank) String() (out string) {
+	for _, battery := range bank.Batteries {
+		out += fmt.Sprint(battery.Joltage)
+	}
+
+	return out
 }
