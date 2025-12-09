ea1a1d1a0bd66b3437ee7092a5a3d37464498523
Baipyrus <simon.waltemathe@gmail.com>
2025-12-09 16:56:26 +0100

    feat: implement solution for challenge day04 part2

--- internal/day04/part2/main.go
+++ internal/day04/part2/main.go
@@ -16,27 +16,53 @@ func init() {
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
 
-	var paperRolls uint
+	var (
+		paperRolls uint
+		newGrid    day04.Grid
+	)
+
 	grid := day04.ParseInput(input)
+	gridHasChanged := true
 
-	for y := range grid.Height {
-		for x := range grid.Width {
-			cell := grid.GetCell(x, y)
-			if cell.Type != '@' {
-				continue
-			}
+	// Lazy solution: simply redo this loop
+	// to scan the entire grid again and
+	// find newly accessible paper rolls.
+	for gridHasChanged {
+		// Assume this will be the last iteration:
+		// The grid will no longer change, the loop
+		// will end after this final pass.
+		gridHasChanged = false
+		newGrid = grid
 
-			neighbors := grid.GetNeighbors(x, y, false, true)
-
-			var neighborCount uint
-			for _, n := range neighbors {
-				if n.Type == '@' {
-					neighborCount++
+		for y := range grid.Height {
+			for x := range grid.Width {
+				cell := grid.GetCell(x, y)
+				if cell.Type != '@' {
+					continue
 				}
-			}
 
-			if neighborCount < 4 {
-				paperRolls++
+				neighbors := grid.GetNeighbors(x, y, false, true)
+
+				var neighborCount uint
+				for _, n := range neighbors {
+					if n.Type == '@' {
+						neighborCount++
+					}
+				}
+
+				if neighborCount < 4 {
+					paperRolls++
+
+					// Removing the old paper roll that
+					// has been removed by the forklift
+					cell.Type = '.'
+					newGrid.SetCell(x, y, cell)
+
+					// Another accessible paper roll
+					// has been found! We need to redo
+					// the loop and find new spots.
+					gridHasChanged = true
+				}
 			}
 		}
 	}
