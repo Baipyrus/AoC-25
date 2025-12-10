ccf6a4411541efe8afa9d07e6c1064cdf70e45b0
Baipyrus <simon.waltemathe@gmail.com>
2025-12-10 16:26:03 +0100

    feat: implement solution for challenge day05 part1

--- internal/day05/part1/main.go
+++ internal/day05/part1/main.go
@@ -2,6 +2,8 @@ package day05_part1
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/Baipyrus/AoC-25/internal/registry"
 )
@@ -14,4 +16,26 @@ func init() {
 
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
+
+	segments := strings.Split(input, "\n\n")
+	freshRanges := strings.Split(strings.TrimSpace(segments[0]), "\n")
+	ingredients := strings.Split(strings.TrimSpace(segments[1]), "\n")
+
+	var freshIngredients uint64
+	for _, ingredient := range ingredients {
+		ingredientId, _ := strconv.ParseUint(ingredient, 10, 64)
+
+		for _, freshRange := range freshRanges {
+			bounds := strings.Split(freshRange, "-")
+			lower, _ := strconv.ParseUint(bounds[0], 10, 64)
+			upper, _ := strconv.ParseUint(bounds[1], 10, 64)
+
+			if ingredientId >= lower && ingredientId <= upper {
+				freshIngredients++
+				break
+			}
+		}
+	}
+
+	fmt.Printf("There are a total of %d fresh ingredients left.\n", freshIngredients)
 }
