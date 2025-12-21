43697b2afdd3f629aeb3b8e8472b7e594258b96a
Baipyrus <simon.waltemathe@gmail.com>
2025-12-21 20:08:14 +0100

    feat: implement solution for challenge day09 part1

--- internal/day09/part1/main.go
+++ internal/day09/part1/main.go
@@ -2,7 +2,9 @@ package day09_part1
 
 import (
 	"fmt"
+	"slices"
 
+	"github.com/Baipyrus/AoC-25/internal/day08"
 	"github.com/Baipyrus/AoC-25/internal/registry"
 )
 
@@ -14,4 +16,17 @@ func init() {
 
 func Main(input string) {
 	fmt.Printf("Executing: %s\n", name)
+
+	boxes := day08.ParseInput(input)
+	furthest := day08.Combinations(boxes)
+
+	// Sort all combinations such that we can start with
+	// the two points that are furthest from each other!
+	slices.SortFunc(
+		furthest,
+		func(a, b day08.Rectangle) int {
+			return int(b.Area - a.Area)
+		})
+
+	fmt.Printf("The largest area of any of the available rectangles is: %d\n", furthest[0].Area)
 }
